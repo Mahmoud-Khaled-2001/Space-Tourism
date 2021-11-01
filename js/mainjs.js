dfb05b6562a3aa5ba4f1a04701a8549b12885659
Mahmoud-Khaled-2001 <89226985+Mahmoud-Khaled-2001@users.noreply.github.com>
2021-11-02 00:45:07 +0200

    New JS file

--- js/mainjs.js
+++ js/mainjs.js
@@ -2,28 +2,28 @@
 const moon = {
 	name: 'MOON',
 	p: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.`,
-	url: '../images/destination/image-moon.png',
+	url: './images/destination/image-moon.png',
 	span1: '384,400 km',
 	span2: '3 days'
 };
 const mars = {
 	name: 'MARS',
 	p: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!`,
-	url: '../images/destination/image-mars.png',
+	url: './images/destination/image-mars.png',
 	span1: '225 MIL. km',
 	span2: '9 months'
 };
 const europa = {
 	name: 'EUROPA',
 	p: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.`,
-	url: '../images/destination/image-europa.png',
+	url: './images/destination/image-europa.png',
 	span1: '628 MIL. km',
 	span2: '3 years'
 };
 const titan = {
 	name: 'TITAN',
 	p: `The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.`,
-	url: '../images/destination/image-titan.png',
+	url: './images/destination/image-titan.png',
 	span1: '1.6 BIL. km',
 	span2: '7 years'
 };
@@ -33,25 +33,25 @@ let destArr = [ moon, mars, europa, titan ];
 const douglas = {
 	name: 'Douglas Hurley',
 	p: `Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.`,
-	url: '../images/crew/image-douglas-hurley.png',
+	url: './images/crew/image-douglas-hurley.png',
 	rule: 'Commander'
 };
 const mark = {
 	name: 'MARK SHUTTLEWORTH',
 	p: `Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.`,
-	url: '../images/crew/image-mark-shuttleworth.png',
+	url: './images/crew/image-mark-shuttleworth.png',
 	rule: 'Mission Specialist'
 };
 const victor = {
 	name: 'Victor Glover',
 	p: `Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. `,
-	url: '../images/crew/image-victor-glover.png',
+	url: './images/crew/image-victor-glover.png',
 	rule: 'PILOT'
 };
 const anousheh = {
 	name: 'Anousheh Ansari',
 	p: `Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. `,
-	url: '../images/crew/image-anousheh-ansari.png',
+	url: './images/crew/image-anousheh-ansari.png',
 	rule: 'Flight Engineer'
 };
 
@@ -61,20 +61,20 @@ let crewData = [ douglas, mark, victor, anousheh ];
 const launchVehicle = {
 	name: 'LAUNCH VEHICLE',
 	p: `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!`,
-	urlOne: '../images/technology/image-launch-vehicle-portrait.jpg',
-	urlTwo: '../images/technology/image-launch-vehicle-landscape.jpg'
+	urlOne: './images/technology/image-launch-vehicle-portrait.jpg',
+	urlTwo: './images/technology/image-launch-vehicle-landscape.jpg'
 };
 const spaceport = {
 	name: 'SPACEPORT',
 	p: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.`,
-	urlOne: '../images/technology/image-spaceport-portrait.jpg',
-	urlTwo: '../images/technology/image-spaceport-landscape.jpg'
+	urlOne: './images/technology/image-spaceport-portrait.jpg',
+	urlTwo: './images/technology/image-spaceport-landscape.jpg'
 };
 const spaceCapsule = {
 	name: 'SPACE CAPSULE',
 	p: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.`,
-	urlOne: '../images/technology/image-space-capsule-portrait.jpg',
-	urlTwo: '../images/technology/image-space-capsule-landscape.jpg'
+	urlOne: './images/technology/image-space-capsule-portrait.jpg',
+	urlTwo: './images/technology/image-space-capsule-landscape.jpg'
 };
 
 let tecnoDate = [ launchVehicle, spaceport, spaceCapsule ];
